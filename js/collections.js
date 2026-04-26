8fef5b0d159f1657a982e77cd814a321b56d37d0
Compute101 <82841197+Compute101@users.noreply.github.com>
2026-04-26 20:47:47 +0100

    Update collections.js
    Another fix for the saving collections

--- js/collections.js
+++ js/collections.js
@@ -324,6 +324,7 @@ function showAddModelToList(listId) {
 // --- Collection form ---
 
 function showCollectionForm(editId = null) {
+  editId = editId || null;
   const col = editId ? appData.collections[editId] : null;
 
   const content = document.createElement('div');
